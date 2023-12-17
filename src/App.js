8c9968af2e3efbeb0da3f2f061d4b1cce504b7ff
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2023-12-17 15:40:46 +0600

    Build static design for Form components.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FrindsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -63,7 +66,43 @@ function Friend({ friend }) {
         </p>
       )}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👬 Friend Name</label>
+      <input type="text" />
+
+      <label>🖼️ Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split Bill With X</h2>
+      <label>💰 Bill Value</label>
+      <input type="text" />
+      <label>🕴 Your Expanse</label>
+      <input type="text" />
+      <label>👬 X's Expanse</label>
+      <input type="text" disabled />
+      <label>🤑 Who's paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+    </form>
   );
 }
